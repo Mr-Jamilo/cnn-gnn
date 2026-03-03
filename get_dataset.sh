bba45cf7e621c0139afccc4056ad69f92852ace3
Mr-Jamilo <lokhei@hotmail.co.uk>
2026-03-03 00:11:46 +0000

    updated script

--- get_dataset.sh
+++ get_dataset.sh
@@ -4,4 +4,6 @@ curl -L -o ./retinal-disease-classification.zip\
 
 unzip -o ./retinal-disease-classification.zip -d ./dataset
 rm ./retinal-disease-classification.zip
-
+pip install torch==2.8.0 torchvision==0.23.0 --index-url https://download.pytorch.org/whl/cu126
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.8.0+cu126.html
+pip install torchinfo scikit-learn matplotlib pillow torchmetrics timm
