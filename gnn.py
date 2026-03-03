caf7f4817215b9ce772f954d13213c6b0cc64b2c
Mr-Jamilo <lokhei@hotmail.co.uk>
2026-03-03 11:49:45 +0000

    updated gnn

--- gnn.py
+++ gnn.py
@@ -14,7 +14,7 @@ from sklearn.metrics import classification_report
 from datetime import datetime
 from torchinfo import summary
 from torch_geometric import nn as pyg_nn
-from timm.models.layers import DropPath
+from timm.layers.drop import DropPath
 
 TRAIN_DIR = 'dataset/Training_Set/Training_Set'
 TRAIN_LABELS = pd.read_csv(f'{TRAIN_DIR}/RFMiD_Training_Labels.csv')
@@ -147,63 +147,10 @@ class ViGBlock(nn.Module):
         x = self.fnn(x)
         return x
 
-# class TwoLayerNN(nn.Module):
-#     def __init__(self, in_features, hidden_features=None, out_features=None):
-#         super().__init__()
-#         out_features = out_features or in_features
-#         hidden_features = hidden_features or in_features
-#
-#         self.layer = nn.Sequential(
-#             nn.Linear(in_features, hidden_features),
-#             nn.BatchNorm1d(hidden_features),
-#             nn.GELU(),
-#             nn.Linear(hidden_features, out_features),
-#         )
-#
-#     def forward(self, x):
-#         return self.layer(x) + x
-#
-# class Block(nn.Module):
-#     def __init__(self, in_features, num_edges=9, head_num=1):
-#         super().__init__()
-#         self.k = num_edges
-#         self.num_edges = num_edges
-#         self.in_layer1 = TwoLayerNN(in_features)
-#         self.out_layer1 = TwoLayerNN(in_features)
-#         self.droppath1 = nn.Identity()  # DropPath(0)
-#         self.in_layer2 = TwoLayerNN(in_features, in_features*4)
-#         self.out_layer2 = TwoLayerNN(in_features, in_features*4)
-#         self.droppath2 = nn.Identity()  # DropPath(0)
-#         self.multi_head_fc = nn.Conv1d(in_features*2, in_features, 1, 1, groups=head_num)
-#
-#     def forward(self, x):
-#         B, N, C = x.shape
-#
-#         sim = x @ x.transpose(-1, -2)
-#         graph = sim.topk(self.k, dim=-1).indices
-#
-#         shortcut = x
-#         x = self.in_layer1(x.view(B * N, -1)).view(B, N, -1)
-#
-#         # aggregation
-#         neibor_features = x[torch.arange(B).unsqueeze(-1).expand(-1, N).unsqueeze(-1), graph]
-#         x = torch.stack([x, (neibor_features - x.unsqueeze(-2)).amax(dim=-2)], dim=-1)
-#
-#         # update
-#         # Multi-head
-#         x = self.multi_head_fc(x.view(B * N, -1, 1)).view(B, N, -1)
-#
-#         x = self.droppath1(self.out_layer1(nn.functional.gelu(x).view(B * N, -1)).view(B, N, -1))
-#         x = x + shortcut
-#
-#         x = self.droppath2(self.out_layer2(nn.functional.gelu(self.in_layer2(x.view(B * N, -1)))).view(B, N, -1)) + x
-#
-#         return x
-#
 class ViGNN(nn.Module):
     def __init__(self, in_features=3*16*16, out_feature=320, num_patches=196, num_ViGBlocks=16, num_edges=9, head_num=1):
         super().__init__()
-        self.patchifier = CreatePatches()
+        self.patchifier = CreatePatches(in_channels=in_features, patch_size=16, embed_dim=out_feature)
         self.pose_embedding = nn.Parameter(torch.randn(1, num_patches, out_feature))
         self.blocks = nn.Sequential(*[ViGBlock(out_feature, num_edges, head_num) for _ in range(num_ViGBlocks)])
 
