8a71757d6c9926eb2ddd8e68306ec2e4c7c6557f
Jamie Lo <lokhei@hotmail.co.uk>
2026-03-09 23:19:58 +0000

    updated run script for automation

--- run.sh
+++ run.sh
@@ -2,4 +2,13 @@
 
 source .venv/bin/activate
 
-python3 cnn.py --id ""
+python3 test.py --size "tiny" \
+  --dataset_directory "./dataset" \
+  --learning_rate 1e-4 \
+  --weight_decay 1e-3 \
+  --threshold 0.5 \
+  --epochs 150 \
+  --stochastic_path 0.1 \
+  --k_neighbours 9 \
+  --batch_size 64 \
+  --seed 42 \
