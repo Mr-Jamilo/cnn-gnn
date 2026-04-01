912ea8ee0883cbf3fba5422cc3986d05bada4b77
Jamie Lo <lokhei@hotmail.co.uk>
2026-04-01 15:56:02 +0100

    updated run.sh script

--- run.sh
+++ run.sh
@@ -1,32 +1,91 @@
 #!/bin/bash
-#python3 gnn.py --size "small" --dataset_directory "./dataset" --learning_rate 1e-4 --weight_decay 1e-3 --threshold 0.5 --graph_layer_type "GAT" --epochs 150 --stochastic_path 0.1 --k_neighbours 9 --batch_size 16 --seed 42
 
 source .venv/bin/activate
 
-types=("GCN" "GAT" "GIN")
-sizes=("tiny" "small")
-knn_arr=(3 5 7 9)
-sto_path=0
+run_cnn_binary() {
+    local m_res_blocks=$1
 
-for type in "${types[@]}"; do
-  for size in "${sizes[@]}"; do
-    if [ $size = "big" ]; then
-      sto_path=0.3
-    else
-      sto_path=0.1
-    fi
-    for knn in "${knn_arr[@]}"; do
-      python3 gnn.py --size $size \
+    python3 cnn-binary.py \
         --dataset_directory "./dataset" \
         --learning_rate 1e-4 \
         --weight_decay 1e-3 \
         --threshold 0.5 \
-        --graph_layer_type $type \
         --epochs 150 \
-        --stochastic_path $sto_path \
-        --k_neighbours $knn \
-        --batch_size 64 \
+        --batch_size 32 \
+        --seed 42 \
+        --cnn_res_blocks "$m_res_blocks"
+}
+
+run_gnn() {
+    local m_type=$1
+    local m_size=$2
+    local m_knn=$3
+    local m_sto=$4
+
+    python3 gnn-multilabel.py --size "$m_size" \
+        --dataset_directory "./dataset" \
+        --learning_rate 1e-5 \
+        --weight_decay 1e-3 \
+        --threshold 0.6 \
+        --graph_layer_type "$m_type" \
+        --epochs 150 \
+        --stochastic_path "$m_sto" \
+        --k_neighbours "$m_knn" \
+        --batch_size 32 \
         --seed 42
-    done
-  done
-done
+} 
+
+run_cnn_gnn() {
+    local m_type=$1
+    local m_size=$2
+    local m_knn=$3
+    local m_sto=$4
+    local m_extract=$5
+    local m_res_blocks=$6
+
+    python3 cnn-gnn-binary.py --size "$m_size" \
+        --dataset_directory "./dataset" \
+        --learning_rate 4e-4 \
+        --weight_decay 1e-3 \
+        --threshold 0.5 \
+        --graph_layer_type "$m_type" \
+        --epochs 150 \
+        --stochastic_path "$m_sto" \
+        --k_neighbours "$m_knn" \
+        --batch_size 32 \
+        --seed 42 \
+        --cnn_extraction_layer "$m_extract" \
+        --cnn_res_blocks "$m_res_blocks"
+}
+
+run_cnn_gnn_multilabel() {
+    local m_type=$1
+    local m_size=$2
+    local m_knn=$3
+    local m_sto=$4
+    local m_extract=$5
+    local m_res_blocks=$6
+
+    python3 cnn-gnn-multilabel.py --size "$m_size" \
+        --dataset_directory "./dataset" \
+        --learning_rate 1e-5 \
+        --weight_decay 1e-3 \
+        --threshold 0.5 \
+        --graph_layer_type "$m_type" \
+        --epochs 150 \
+        --stochastic_path "$m_sto" \
+        --k_neighbours "$m_knn" \
+        --batch_size 32 \
+        --seed 42 \
+        --cnn_extraction_layer "$m_extract" \
+        --cnn_res_blocks "$m_res_blocks"
+}
+
+# Examples:
+# run_cnn_binary "3,4,6,3"
+# run_gnn "GCN" "medium" 7 0.1
+# run_cnn_gnn "GCN" "medium" 7 0.1 "layer4" "3,4,6,3"
+# run_cnn_gnn_multilabel "GCN" "medium" 7 0.1 "layer4" "3,4,6,3"
+# run_gnn "GCN" "medium" 7 0.1
+
+run_cnn_gnn "GCN" "big" 7 0.1 "layer4" "3,4,6,3"
