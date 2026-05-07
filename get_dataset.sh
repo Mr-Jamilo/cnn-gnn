3ab2e234baf16515af18ed3fe178070826328e18
Jamie Lo <lokhei@hotmail.co.uk>
2026-05-07 10:51:35 +0100

    updated script

--- get_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -L -o ./retinal-disease-classification.zip\
-  https://www.kaggle.com/api/v1/datasets/download/andrewmvd/retinal-disease-classification
-
-unzip -o ./retinal-disease-classification.zip -d ./dataset
-rm ./retinal-disease-classification.zip
-pip install torch==2.8.0 torchvision==0.23.0 --index-url https://download.pytorch.org/whl/cu126
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.8.0+cu126.html
-pip install torchinfo scikit-learn matplotlib pillow torchmetrics timm
---
+++ prerequisites.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -L -o ./retinal-disease-classification.zip https://www.kaggle.com/api/v1/datasets/download/andrewmvd/retinal-disease-classification
+
+unzip -o ./retinal-disease-classification.zip -d ./dataset
+rm ./retinal-disease-classification.zip
+pip install torch==2.11.0 torchvision==0.26.0 --index-url https://download.pytorch.org/whl/cu126
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.11.0+cu126.html
+pip install torchinfo scikit-learn matplotlib pillow torchmetrics timm
